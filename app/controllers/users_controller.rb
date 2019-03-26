5bd8c8a1012412ea940694922bc329d29d4b18fc
github username <github email address>
2019-03-26 16:47:15 +0000

    Kai leaderboard, poll create get points merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,4 @@ class UsersController < ApplicationController
    @user = User.find(params[:id])
  end
 
- def new
-   @user = User.new
- end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :followers, only: [:index]
+  resources :followers, only: [:index, :create]
   resources :polls, :users
   resources :votes, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "users#new"
+  root "polls#index"
 
 end
