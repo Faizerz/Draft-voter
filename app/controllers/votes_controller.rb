2fbeb408571257d3987d16c686645efd941daae8
KaiKaspar <kai.k.b.shovlin@gmail.com>
2019-03-26 09:07:33 +0000

    dropdown menu

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,6 +4,7 @@ class VotesController < ApplicationController
    voting_user = User.find_by(username: current_user.username)
    params[:button] == 'true' ? params[:button] = true : params[:button] = false
    @vote = Vote.create(user_id: voting_user.id, poll_id: params[:poll_id], vote: params[:button])
+   current_user.score += 1
  end
 
 end
