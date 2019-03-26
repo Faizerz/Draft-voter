789fda6c3f7e24ca5655597acaef899909522de6
KaiKaspar <kai.k.b.shovlin@gmail.com>
2019-03-26 16:31:27 +0000

    poll creator gets points

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,11 +1,20 @@
 class VotesController < ApplicationController
   def create
     voting_user = User.find_by(username: current_user.username)
-    # poll_creator = User.find_by(username: poll.username)
-    params[:button] = params[:button] == 'true'
-    @vote = Vote.create(user_id: voting_user.id, poll_id: params[:poll_id], vote: params[:button])
-    current_score = voting_user.score
-    voting_user.update(score: (current_score + 2))
-    # poll_creator.update(score: (current_score + 1))
+    @vote = Vote.create(user_id: voting_user.id, poll_id: params[:poll_id].to_i, vote: params[:button])
+
+
+
+    voted_on_poll_id = params[:poll_id].to_i
+    poll_user_id = Poll.find(voted_on_poll_id).user_id
+    poll_creator = User.find_by(id: poll_user_id)
+
+
+    voter_current_score = voting_user.score
+    creator_current_score = poll_creator.score
+
+    voting_user.update(score: (voter_current_score + 2))
+    poll_creator.update(score: (creator_current_score + 1))
+    redirect_to polls_path
   end
 end
